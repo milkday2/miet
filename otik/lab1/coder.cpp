f5a94caa4568decbb7c077ae56652d30cf909d22
milkday2 <131621546+milkday2@users.noreply.github.com>
2023-10-07 06:08:51 +0300

    Update coder.cpp

--- otik/lab1/coder.cpp
+++ otik/lab1/coder.cpp
@@ -1,70 +1,75 @@
 #include <cstdint>
 #include <fstream>
-#include <ios>
 #include <iostream>
 #include <string>
+#include <vector>
+#include "kamd.h"
+#include "otikutils.h"
 
-void coder(std::string, uint16_t, uint16_t);
+
+void coder(std::string,std::string output, kamd_header *header);
 
 int main(int argc, char *argv[]) {
   if (argc < 2) {
-    std::cout << "command should include a file name argument\n";
+    std::cout << "command should include a file name and an output name\n";
     return 0;
   }
   std::string path(argv[1]);
-  coder(path,0,0);
+  std::string output(argv[2]);
+  kamd_header header;
+  coder(path, output, &header);
   return 0;
 }
 
-void coder(std::string path, uint16_t compression_method,
-           uint16_t filtering_method) {
-  std::ifstream fileIn(path, std::ios::in | std::ios::binary);
+void coder(std::string path,std::string output, kamd_header* header) {
+  std::ifstream fileIn(path.c_str(), std::ios::in | std::ios::binary);
   if (!fileIn.good()) {
     std::cout << "File not found" << std::endl;
     return;
   }
 
-  uint32_t filesize = 0;
-  filesize = fileIn.tellg();
+  header->file_size = fileIn.tellg();
   fileIn.seekg(0, std::ios::end);
-  filesize = (uint32_t)fileIn.tellg() - filesize;
+  header->file_size = (uint32_t)fileIn.tellg() - header->file_size;
   fileIn.seekg(0, std::ios::beg);
 
-  char *buf = new char[filesize];
-  fileIn.read(buf, filesize);
-  fileIn.close();
 
-  std::string base_filename = path.substr(path.find_last_of("/\\") + 1);
-  std::string::size_type const p(base_filename.find_last_of('.'));
-  std::string file_without_extension = base_filename.substr(0, p);
-  if (p == 0)
-    file_without_extension = base_filename;
+  std::string filename = path.substr(path.find_last_of("/\\") + 1);
+  header->filename_size = filename.length();
 
-  std::ofstream fileOut(file_without_extension + ".kamd",
-                        std::ios::out | std::ios::binary);
+  std::ofstream fileOut(output.c_str(), std::ios::out | std::ios::binary);
   if (!fileOut.good()) {
-    std::cout << "Error opening file" << std::endl;
+    std::cout << "couldn't open/create " << output << '\n';
     return;
   }
-  const char* signature = "\x4B\x61\x4D\x64";
-  const char* version = "\x01\x00";
-  uint16_t compression_data_size = 0;
-  uint16_t filtering_data_size = 0;
-  uint16_t filename_size = base_filename.length();
-  fileOut.write(signature, 4);
-  fileOut.write(version, 2);
-  fileOut.write(reinterpret_cast<const char *>(&compression_method), 2);
-  fileOut.write(reinterpret_cast<const char *>(&filtering_method), 2);
-  fileOut.write(reinterpret_cast<const char *>(&filesize), 4);
-  fileOut.write(reinterpret_cast<const char *>(&filename_size), 2);
-  fileOut.write(reinterpret_cast<const char *>(&compression_data_size), 2);
-  fileOut.write(reinterpret_cast<const char *>(&filtering_data_size), 2);
-  fileOut.write(base_filename.c_str(), filename_size);
 
-  fileOut.write(buf, filesize);
+  fileOut.write((char*) header->signature, 4);
+  fileOut.write((char*) header->version, 2);
+  fileOut.write((char*) header->comp_method, 2);
+  fileOut.write((char*) header->intf_protection, 2);
+  fileOut.write((char*)int2lendian(header->file_size), 4);
+  fileOut.write((char*)sint2lendian(header->filename_size), 2);
+  fileOut.write((char*)sint2lendian(header->c_overhead_size), 2);
+  fileOut.write((char*)sint2lendian(header->i_overhead_size), 2);
+  fileOut.write(filename.c_str(), header->filename_size);
+  if (header->c_overhead_size) {
+      // write overhead
+  }
+  if (header->i_overhead_size) {
+      // write overhead
+  }
 
-  delete[] buf;
-
+  int chunk_size = header->file_size > 64 * 1024 ? 64 * 1024 : header->file_size;
+  std::vector<unsigned char> buffer(chunk_size);
+  while (fileIn.good()) {
+      fileIn.read((char*)buffer.data(), chunk_size);
+      std::streamsize s = fileIn.gcount();
+      if (s > 0) {
+          fileOut.write((char*)buffer.data(), s);
+      }
+  }
+  fileIn.close();
   fileOut.close();
 }
+
 
